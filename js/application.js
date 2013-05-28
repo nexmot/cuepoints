715d03d54b1c80dcbb6b5a39e9b42a960af4e482
Ignacio de Juan <ignaciodejuan@gmail.com>
2013-05-29 01:58:03 +0200

    fixed loading new videos, append and remove all cuepoints, text will be seen in prompter tag

--- js/application.js
+++ js/application.js
@@ -8,25 +8,29 @@ $(document).ready(function() {
     
   // $('h1').text('y esto funciona?'); 
   // embed full url example http://www.youtube.com/embed/S9dqJRyk0YM
-   $('#video_url').val("http://www.youtube.com/watch?v=cRmbwczTC6E");
-   var video_id = $('#video_url').val();
-     
+    $('#video_url').val("http://www.youtube.com/watch?v=cRmbwczTC6E");
+    var video_id = $('#video_url').val();
+    var ytplayer; 
    
    $('button#submit_video').on('click', function() {
-      
+      console.log('pressed submit');
       var video_id = youtube_parser($('#video_url').val());
+ //     console.log(video_id);
       
       //var video_url_embed = $('#video_url').val();
       //$('#video').html('<iframe id="player" origin="http://localhost.php-apps" width="560" height="315" src="'+video_url_embed+'" frameborder="0" allowfullscreen></iframe>');
       //$('#video').html(iframe);     
     function onYouTubeIframeAPIReady() { 
-      player = new YT.Player('player', {
+              console.log(video_id);
+
+              if (ytplayer != null) { ytplayer.destroy();}
+      ytplayer = new YT.Player('player', {
                 height: '390',
                 width: '580',
                 videoId: video_id,
                 events: {
-                  'onReady': onPlayerReady,
-                  //'onStateChange': onPlayerStateChange
+                  //'onReady': onPlayerReady,
+                  'onStateChange': onPlayerStateChange
                 }
               }); 
     };
@@ -49,16 +53,16 @@ $(document).ready(function() {
 
    var get_cuepoint_item = function (time) {
      
-     time = time || player.getCurrentTime();
-     console.log(time);
+     time = time || ytplayer.getCurrentTime();
+     //console.log(time);
        var cuepointtext = $('input#type_text').val();
        return $('<tr class="cuepoints" data-cuepointtime="'+ time +'" data-cuepointtext="'+cuepointtext+'"><td><a id="seek_point">'+formatTimestamp(time)+'</a></td><td><form action=""><textarea id="submit_text" class="cuepoint_item" type="text" placeholder="enter text" >'+cuepointtext+'</textarea></form><td><button id="remove" class="small alert button">remove</button><button id="add_next" class="small button">add next</button><button id="save_cuepoint" class="small button" style="display:none">save cuepoint</button></td></tr>'); 
        
    };
    
    function addCuepoint (time) {
-        console.log(player.getCurrentTime());
-        console.log(time);
+     //   console.log(ytplayer.getCurrentTime());
+     //   console.log(time);
         var cuepoint_item = get_cuepoint_item(time);
         $('tbody#timer').append(cuepoint_item);  
 
@@ -66,13 +70,26 @@ $(document).ready(function() {
       
    function addNextCuepoint () {
 
-       console.log(player.getCurrentTime());
+      // console.log(ytplayer.getCurrentTime());
 
        var cuepoint_item = get_cuepoint_item();
        $(this).closest('tr').after(cuepoint_item);  
        console.log($(this));
 
    };
+   
+   function removeCuepoint() {
+ 
+           (confirm('are you sure you want to delete this cue point?')) ? $(this).closest('tr').remove() : null  ;      
+
+     };
+     
+   
+
+             
+
+
+   
     
    function saveCuepoint() {
         var $tr = $(this).closest('tr');        
@@ -124,7 +141,7 @@ $(document).ready(function() {
    var time_init_typing;
    
    $('input#type_text').on('keypress', function(e) {
-       time_init_typing = time_init_typing || player.getCurrentTime();
+       time_init_typing = time_init_typing || ytplayer.getCurrentTime();
        if(e.which == 13) {
                addCuepoint(time = time_init_typing);
                $('input#type_text').val("");
@@ -134,19 +151,53 @@ $(document).ready(function() {
     
    $('button#add_cuepoint').on('click', function() {
        
-       var time = player.getCurrentTime();
+       var time = ytplayer.getCurrentTime();
        addCuepoint(time);
    });
+   
+   $('button#add_cuepoint_all').on('click', function() {
+       
+       var number_cuepoints = Math.floor(ytplayer.getDuration() / 5);
+       console.log(number_cuepoints);
+       
+       var cuepointtime = 0;
+       
+       for (var i = 0; i < number_cuepoints; i++) {
+       cuepointtime += 5;   
+       console.log(cuepointtime);
+       addCuepoint(cuepointtime);    
+           
+       };
+       
+       
+   });
+   
+   $('button#remove_cuepoint_all').on('click', function() {
+          var $cuepoints = $('.cuepoints');
+          var number_cuepoints = $cuepoints.length;
+          console.log(number_cuepoints);
+          if (confirm('are you sure you want to delete ALL the cue points?')) {
+          
+              
+                for (var i = 0; i < number_cuepoints; i++) {
+
+                $cuepoints.eq(i).remove();    
+
+                };
+              
+              
+          };
+          
+
+
+      });
+   
     
    $('button#generate_srt').on('click', generateSrt);
    
-   $(document).on('click','button#remove', function() {
-        // console.log('button clicked');
-
-        (confirm('are you sure you want to delete this cue point?')) ? $(this).closest('tr').remove() : null  ;      
-
-   });
-
+   $(document).on('click','button#remove',removeCuepoint);
+   
+   
    $(document).on('click', 'button#add_next', addNextCuepoint);  
     
    $(document).on('click', 'button#save_cuepoint', saveCuepoint);
@@ -158,7 +209,7 @@ $(document).ready(function() {
    });
 
 
-   $(document).on('blur', 'input', function() {
+   $(document).on('blur', 'textarea', function() {
 
        $(this).removeClass('highlighted');
 
@@ -167,11 +218,59 @@ $(document).ready(function() {
    $(document).on('click', 'a#seek_point', function() {
 
        var seconds = +$(this).closest('tr').data('cuepointtime');
-       player.seekTo(seconds, true);
+       ytplayer.seekTo(seconds, true);
 
    });
      
    
+   //http://stackoverflow.com/questions/7298341/timer-along-with-youtube-clip-with-js-to-show-divs-at-different-times
+
+
+   function onPlayerStateChange(event) {    
+       
+       if(event.data == YT.PlayerState.PLAYING){ // playing
+            startWatch();
+            console.log("playing");
+       }
+       else if(YT.PlayerState.ENDED || YT.PlayerState.PAUSED){ //ended or paused
+            stopWatch();   
+       }            
+   }
+
+   var overlays = [
+       {text:"Steve Chen",from:1,to:6},
+       {text:"Geoff Stearns",from:8,to:14}]
+
+   var watchId;
+
+   var $cuepoints;
+   
+   function startWatch(){
+       watchId = setInterval( function(){
+           showOrHideOverlay(ytplayer.getCurrentTime());
+       },1000);
+       $cuepoints = $('.cuepoints');
+   }
+
+   function stopWatch(){
+     clearTimeout(watchId);
+   }
+
+   function showOrHideOverlay(time){
+       // find overlays which should be visible at "time"
+       var shownOverlays = false;
+       for(var i=0;i<$cuepoints.length;i++){
+           cuepointtext = $('.cuepoints').eq(i).closest('tr').data('cuepointtext');
+           cuepointtime = $('.cuepoints').eq(i).closest('tr').data('cuepointtime');
+           if(cuepointtime < time && cuepointtime + 5 > time){
+               $('#prompter').text(cuepointtext);
+               shownOverlays = true;
+           }           
+       }
+       if(!shownOverlays)
+           $('#prompter').text("");
+
+   }
     
 });
 
